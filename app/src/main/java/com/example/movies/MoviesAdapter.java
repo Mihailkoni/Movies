6295a062124e9d7646fd66e7128c98fd69a9b4b1
Mihail <korikov.me@students.dvfu.ru>
2025-04-02 00:11:41 +1000

    I removed the rating display from the layout (so that it could be installed in the adapter). I made the rating display in the adapter and rounded the rating to one decimal place.

--- app/src/main/java/com/example/movies/MoviesAdapter.java
+++ app/src/main/java/com/example/movies/MoviesAdapter.java
@@ -1,6 +1,7 @@
 package com.example.movies;
 
 import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -42,7 +44,18 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
                 .load(movie.getPoster().getUrl())
                 .into(holder.imageViewPoster);
 
-        holder.textViewRating.setText(movie.getRating().getKp());
+        double rating = movie.getRating().getKp();
+        int backgroundId;
+        if(rating > 7){
+            backgroundId = R.drawable.circle_green;
+        } else if (rating > 5) {
+            backgroundId = R.drawable.circle_yellow;
+        } else {
+            backgroundId = R.drawable.circle_red;
+        }
+        Drawable background = ContextCompat.getDrawable(holder.itemView.getContext(),backgroundId);
+        holder.textViewRating.setBackground(background);
+        holder.textViewRating.setText(String.valueOf(rating).substring(0,3));
     }
 
     @Override
