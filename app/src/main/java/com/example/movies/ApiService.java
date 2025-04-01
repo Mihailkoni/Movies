cff3e2ce201f2daa016905fc8c8472fa09c467b5
Mihail <korikov.me@students.dvfu.ru>
2025-04-02 00:12:25 +1000

    changed the API request to get the data more correctly.

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -8,6 +8,6 @@ public interface ApiService {
     /** В запрос можно передавать различные параметры динамически
      * с помощью аннотации @Query ("Название параметра" сам параметр).
      */
-    @GET("movie?token=N52YBFW-PNDM406-JARR9FH-C5G0139&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=50&type=anime")
+    @GET("movie?token=N52YBFW-PNDM406-JARR9FH-C5G0139&rating.kp=7-10&sortField=votes.kp&sortType=-1&limit=50&type=anime")
     Single<MovieResponse> loadMovies(@Query("page") int page);
 }
